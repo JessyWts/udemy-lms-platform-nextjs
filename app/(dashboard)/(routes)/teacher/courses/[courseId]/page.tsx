455fe2408c5fe99ee70f8dc3b6e379aaf886c65f
JessyWts <jamz971@gmail.com>
2024-12-28 18:35:54 +0100

    Edit page UI - custom icon badge - next config

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,71 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { LayoutDashboard } from "lucide-react";
+
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+
+const CourseIdPage = async ({
+    params
+}: {
+    params:{ courseId: string }
+}) => {
+    const { userId } = await auth();
+    const {courseId} = await params;
+
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const course = await db.course.findUnique({
+        where:{
+            id: courseId,
+        }
+    
+    });
+
+    if (!course) {
+        return redirect("/");
+    }
+
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+    ];
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+
+    const completionText = `(${completedFields}/${totalFields})`;
+
+    return ( 
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className='flex flex-col gap-y-2'>
+                    <h1 className="text-2xl font-medium">
+                        Course_setup
+                    </h1>
+                    <span className='text-sm text-slate-700'>
+                        Complete all fields {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard}/>
+                        <h2 className="text-xl">
+                            Customize your course
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default CourseIdPage;
