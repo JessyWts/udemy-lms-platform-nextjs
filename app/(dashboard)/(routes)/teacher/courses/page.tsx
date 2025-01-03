fc233218827d9358ca6d2ff4e684975fac2c0df3
JessyWts <jamz971@gmail.com>
2025-01-03 15:48:05 +0100

    List Courses datatble: - add components and packages - implement colums and data tables for display courses from userId - rescues datas - filter formatDateTime -fix redirect sidebar when on the same routes

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,45 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
-const CoursesPage = () => {
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const CoursesPage = async() => {
+    const {userId} = await auth();
+
+    if (!userId) {
+        redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId: userId,
+            teacherId: userId,
+        },
+        include: {
+            chapters:{
+                include: {
+                    muxData: true,
+                }
+            }
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
+    const coursesWithChapters = courses.map((course) => {
+        return {
+            ...course,
+            chapters: course.chapters.length,
+        };
+    });
+
+
     return ( 
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>
-                    New Course
-                </Button>
-            </Link>
+            <DataTable columns={columns} data={coursesWithChapters} />
         </div>
      );
 }
